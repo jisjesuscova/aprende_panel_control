99a90ec547872f5473e3d9006b19e67dd129c41d
Jesús Cova <jesuscova@jisparking.com>
2023-12-25 20:56:10 -0300

    cambios

--- app/Http/Controllers/Api/FrontSectionController.php
+++ app/Http/Controllers/Api/FrontSectionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Section;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FrontSectionController extends Controller
 {
