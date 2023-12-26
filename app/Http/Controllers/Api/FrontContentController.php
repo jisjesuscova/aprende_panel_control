6e76c8ccdba7127a221876a3bfc3c12fa9b206dd
Jesús Cova <jesuscova@jisparking.com>
2023-12-25 21:41:20 -0300

    cambios

--- app/Http/Controllers/Api/FrontContentController.php
+++ app/Http/Controllers/Api/FrontContentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Content;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FrontContentController extends Controller
 {
